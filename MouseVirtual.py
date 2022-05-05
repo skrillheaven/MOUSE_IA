9e8740a30a7d9a4d493d1a3884cb839d0dbe35b8
skrillheaven <beto.jacp2010@gmail.com>
2022-05-05 18:42:00 -0500

    inicializando los parametros de la deteccion , para que cada objeto tenga su varible

--- MouseVirtual.py
+++ MouseVirtual.py
@@ -4,3 +4,11 @@ import mediapipe
 import time
 
 
+class detectormanos():
+    def __init__(self,mode=False, maxManos =2, Confdeteccion =0.5,Confsegui=0.5):
+        self.mode =mode
+        self.maxManos =maxManos
+        self.Confdeteccion = Confdeteccion
+        self.Confsegui = Confsegui
+
+        
