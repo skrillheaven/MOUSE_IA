fa098b568cf5bb1d91770bdae15064ed59a19112
skrillheaven <beto.jacp2010@gmail.com>
2022-05-05 18:57:21 -0500

    creacion de los objetos que detectaran las manos y las dibujaran

--- MouseVirtual.py
+++ MouseVirtual.py
@@ -11,4 +11,11 @@ class detectormanos():
         self.Confdeteccion = Confdeteccion
         self.Confsegui = Confsegui
 
+        self.mpmanos =mp.solutions.hands
+        self.manos = self.mpmanos.Hands(self.mode,self.maxManos,self.Confdeteccion,self.Confsegui)
+        self.dibujo = mp.solutions.drawing_utils
+        self.tip =[4,8,12,16,20]
+
+
+
         
