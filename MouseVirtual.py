7da854b4ce1fe552e2e6a7c6fd6852f6df5bbc5b
skrillheaven <beto.jacp2010@gmail.com>
2022-05-15 20:45:45 -0500

    Declaracion de variables

--- MouseVirtual.py
+++ MouseVirtual.py
@@ -3,5 +3,12 @@ import numpy as np
 import SeguimientoManos as sm
 import autopy
 
+anchocam, altocam=640,480
+cuadro =100
+anchopanta,altopanta =autopy.screensize()
+sua=5
+pubix,pubiy =0,0
+cubix,cubiy =0,0
+
 
 
