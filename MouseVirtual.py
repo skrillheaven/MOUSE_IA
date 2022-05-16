c7229a37da33e5170470f6166aa60d20d4365888
skrillheaven <beto.jacp2010@gmail.com>
2022-05-15 21:14:47 -0500

    hacemos click si la distancia es corta

--- MouseVirtual.py
+++ MouseVirtual.py
@@ -31,35 +31,39 @@ while True:
         x1,y1 =lista[8][1:]
         x2,y2 = lista[12][1:]
         
-    dedos=detector.dedosarriba()
-    cv2.rectangle(frame,(cuadro,cuadro),(anchocam - cuadro,altocam-cuadro),(0,0,0),2)
+        dedos=detector.dedosarriba()
+        cv2.rectangle(frame,(cuadro,cuadro),(anchocam - cuadro,altocam-cuadro),(0,0,0),2)
 
-    if dedos[1]==[1] and dedos [2] ==0:
+        if dedos[1]==[1] and dedos [2] ==0:
 
-        x3= np.interp(x1,(cuadro,anchocam-cuadro),(0,anchopanta))
-        y3 =np.interp(y1,(cuadro,altocam-cuadro),(0,altopanta))
-
-
-        cubix = pubix +(x3-pubix) /sua
-        cubiy = pubiy +(y3-pubiy)/sua
+            x3= np.interp(x1,(cuadro,anchocam-cuadro),(0,anchopanta))
+            y3 =np.interp(y1,(cuadro,altocam-cuadro),(0,altopanta))
 
 
-        autopy.mouse.move(anchopanta-cubix,cubiy)
-        cv2.circle(frame,(x1,y1),10,(0,0,0),cv2.FILLED)
-        pubix,pubiy = cubix,cubiy
+            cubix = pubix +(x3-pubix) /sua
+            cubiy = pubiy +(y3-pubiy)/sua
 
 
-    if dedos [1] == 1 and dedos [2] == 1:
+            autopy.mouse.move(anchopanta-cubix,cubiy)
+            cv2.circle(frame,(x1,y1),10,(0,0,0),cv2.FILLED)
+            pubix,pubiy = cubix,cubiy
 
-        longitud, frame,linea = detector.distancia(8,12,frame)
-        print(longitud)
-        if longitud < 30:
-            cv2.circle(frame,(linea[4],linea[5]),10,(0,255,0),cv2.FILLED)
 
-            autopy.mouse.click()
+        if dedos [1] == 1 and dedos [2] == 1:
 
-cv2.imshow("Mouse",frame)
-k = cv2.waitKey(1)
+            longitud, frame,linea = detector.distancia(8,12,frame)
+            print(longitud)
+            if longitud < 30:
+                cv2.circle(frame,(linea[4],linea[5]),10,(0,255,0),cv2.FILLED)
+
+                autopy.mouse.click()
+
+    cv2.imshow("Mouse",frame)
+    k = cv2.waitKey(1)
+    if k==27:
+        break
+cap.release()
+cv2.destroyAllWindows()
 
 
 
